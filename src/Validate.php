da2de530726974911bca6620dd2ee608641405f3
Juan Mejía <juanes.reich@gmail.com>
2021-08-10 18:02:36 -0500

    Adding information

--- src/Validate.php
+++ src/Validate.php
@@ -3,6 +3,6 @@ namespace App;
 
 class Validate{
     public static function email($value){
-        return  (bool) \filter_var($value, FILTER_VALIDATE_EMAIL);
+        return  (bool) filter_var($value, FILTER_VALIDATE_EMAIL);
     }
 }
