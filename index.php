b38e2a37d66c10221344a8dffe38b605c5e64170
Juan Mejía <juanes.reich@gmail.com>
2021-08-10 16:57:49 -0500

    variables variables

--- index.php
+++ index.php
@@ -1,10 +1,37 @@
 <?php
-/* 
+
 echo 'Un texto de una línea
 varias líneas
 comilla simple \' backslash \\ continuar con mas texto
-$variable';
-*/
+$variable <br>';
 
 $name = 'Juanes';
-echo "mi nombre es $name";
+echo "mi nombre es $name <br>";
+
+$courses = [
+    'backend' => [
+        'PHP',
+        'Laravel'
+    ]
+];
+
+class User{
+    public $name = 'Juan';
+}
+
+$user = new User;
+
+echo "$user->name quire aprender {$courses['backend'] [0]} <br>";
+
+// variable variables
+$teacher = 'italo';
+$italo = 'Profesor de PHP';
+echo "$teacher es ${$teacher} <br>";
+
+function getTeacher(){
+    return 'teacher';
+}
+
+$teacher = 'Italo';
+
+echo "${getTeacher()} enseña PHP";
